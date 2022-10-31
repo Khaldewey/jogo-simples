b2e240b3eea8da951208e6fec47bd7ddf2268610
Khaldewey <israel_alves77@hotmail>
2022-10-31 18:14:08 -0300

    inserção cronômetro

--- js/script.js
+++ js/script.js
@@ -1,11 +1,63 @@
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
+
+
+var sec = 0;
+var min = 0;
+var hra = 0; 
+var interval; 
+
+function refresh(){
+    location.reload();
+}
+
+// CRONÔMETRO 
+
+function doisDigitos(digito){
+    if(digito < 10){
+        return('0'+digito);
+    }else{
+        return(digito);
+    }
+}
+
+function start(){
+    watch();
+    interval = setInterval(watch,10);
+}  
+
+function stop(){
+    sec = 0;
+    min = 0; 
+    clearInterval(interval);
+    document.getElementById('watch').innerText='00:00:00';
+} 
+
+function watch(){
+sec++;
+if(sec==60){
+    min++;
+    sec=0;
+    if(min == 60){
+        hra++;
+        min = 0;
+    }
+}
+document.getElementById('score').innerText=doisDigitos(hra)+':'+doisDigitos(min)+':'+doisDigitos(sec);
+} 
+
+
+// COMPORTAMENTOS 
+
 const jump = () => {
     mario.classList.add('jump');
     setTimeout(() => {
         mario.classList.remove('jump');
     },500);
+    
 } 
+
+
 
 const loop = setInterval(() =>{
 const pipePosition = pipe.offsetLeft;
@@ -24,10 +76,14 @@ if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
     mario.src = './img/game-over.png';
     mario.style.width = '75px';
     mario.style.marginLeft = '50px'; 
+    stop();
+    
+    clearInterval(loop); 
 
-    clearInterval(loop);
 }
 
-},10)
 
+},10) 
+
+start();
 document.addEventListener('keydown',jump);
